4c435dfd501efd47b276ed965b126fcb689a9deb
Casey Catron <caseycatron1@gmail.com>
2016-01-30 15:01:24 -0500

    Initial Commit

---
+++ RedVsBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedVsBlue
+//
+//  Created by Casey Catron on 1/30/16.
+//  Copyright © 2016 Casey Catron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
